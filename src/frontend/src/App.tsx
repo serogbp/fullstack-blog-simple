4ff75599f844d4f5eca7e4a935a050c75a7be465
serogbp <49165676+serogbp@users.noreply.github.com>
2023-06-29 13:03:04 +0200

    Install react-i18next

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,5 +1,11 @@
+import { useTranslation } from "react-i18next";
+
 function App() {
-	return <p>Hello world!</p>;
+	const { t, i18n } = useTranslation();
+
+	const text = t("Welcome to React");
+
+	return <p className="text-3xl text-red-500">{text}</p>;
 }
 
 export default App;
