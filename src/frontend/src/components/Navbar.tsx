4dc268e1ec96fcd844b536bbf07c01ece471b0e5
serogbp <49165676+serogbp@users.noreply.github.com>
2023-07-03 16:44:55 +0200

    Refactor Navbar CSS

--- src/frontend/src/components/Navbar.tsx
+++ src/frontend/src/components/Navbar.tsx
@@ -14,32 +14,12 @@ export default function Navbar() {
 	const { t } = useTranslation();
 
 	return (
-		<Disclosure as="nav" className="bg-gray-800">
-			{({ open }) => (
-				<>
-					<div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-						<div className="relative flex h-16 items-center justify-between">
-							<div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-								{/* Mobile menu button*/}
-								<Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-									<span className="sr-only">Open main menu</span>
-									{open ? <XMarkIcon className="block h-6 w-6" aria-hidden="true" /> : <Bars3Icon className="block h-6 w-6" aria-hidden="true" />}
-								</Disclosure.Button>
-							</div>
-							<div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-								<Link to={"/"} className="flex flex-shrink-0 items-center ">
-									<h1 className="text-3xl font-bold text-stone-50">FullStack blog</h1>
-								</Link>
-							</div>
-							<div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0"></div>
-						</div>
-					</div>
-
-					<Disclosure.Panel className="sm:hidden">
-						<div className="space-y-1 px-2 pb-3 pt-2"></div>
-					</Disclosure.Panel>
-				</>
-			)}
-		</Disclosure>
+		<div className="bg-gray-800">
+			<nav className="mx-auto max-w-7xl p-4">
+				<Link to={"/"} className="flex flex-shrink-0 items-center ">
+					<h1 className="text-3xl font-bold text-stone-50">FullStack blog</h1>
+				</Link>
+			</nav>
+		</div>
 	);
 }
