c657c36fb5e0333041812a7f370d4affa8504eb9
serogbp <49165676+serogbp@users.noreply.github.com>
2023-07-03 15:33:09 +0200

    Footer link

--- src/frontend/src/components/Footer.tsx
+++ src/frontend/src/components/Footer.tsx
@@ -1,13 +1,17 @@
+import { Link } from "react-router-dom";
+
 export default function Footer() {
 	const fechaActual = new Date();
 	const añoActual = fechaActual.getFullYear();
 	const copyrightSymbol = "\u00A9";
 	return (
-		<div className="bg-gray-800 p-4 ">
-			<p className="font-bold text-stone-50">
-				FullStack blog - {copyrightSymbol}
-				{añoActual}
-			</p>
+		<div className="bg-gray-800 p-4">
+			<div className="mx-auto max-w-7xl">
+				<Link to={"/"} className="font-bold text-stone-50">
+					FullStack blog - {copyrightSymbol}
+					{añoActual}
+				</Link>
+			</div>
 		</div>
 	);
 }
